3a3ac6d0e6b9b2060dfd844777ebedd6f69f8eec
Puyan Wei <puyanwei@gmail.com>
2022-11-30 15:56:27 +0000

    Adjust for tied games

--- components/GameOverBanner.tsx
+++ components/GameOverBanner.tsx
@@ -7,20 +7,39 @@ interface GameOverBannerProps {
 
 export function GameOverBanner({ reset, scoreCard }: GameOverBannerProps) {
   function resolveWinner(scoreCard: ScoreCard[]): string {
-    const winningScores = scoreCard.map((player) => player.frames[9].totalScore)
-    const topScore = Math.max(...winningScores)
-    const winningPlayerIndex = winningScores.indexOf(topScore)
+    const finalScores = scoreCard.map((player) => player.frames[9].totalScore)
+
+    if (isTiedTopScores(finalScores)) return `Game over - it's a tie!`
+    const topScore = Math.max(...finalScores)
+    const winningPlayerIndex = finalScores.reverse().indexOf(topScore)
+    // Why is this mapping reversed?
     const winner = scoreCard[winningPlayerIndex].name
-    return winner
+    const winningStatement = `Game over - the winner is ${winner}`
+    return winningStatement
   }
 
-  const winner = resolveWinner(scoreCard)
-  const winningStatement = `Game over - the winner is ${winner}`
+  function isTiedTopScores(array: number[]): boolean {
+    const hasDuplicates = new Set(array).size !== array.length
+    const highestNumberElement = Math.max(...array)
+    const sortedArray = array.sort((a, b) => b - a)
+    const lastElementOfArray = sortedArray[sortedArray.length - 1]
+    const secondToLastElementOfArray = sortedArray[sortedArray.length - 1]
+
+    if (!hasDuplicates) return false
+    if (
+      lastElementOfArray === secondToLastElementOfArray &&
+      lastElementOfArray === highestNumberElement
+    )
+      return true
+    return false
+  }
+
+  const winningText = resolveWinner(scoreCard)
 
   return (
     <div className='text-center'>
       <div className='my-4 text-5xl' data-testid='game-over'>
-        <h2>{winningStatement}</h2>
+        <h2>{winningText}</h2>
       </div>
       <button
         data-testid='button-reset-game-end'
